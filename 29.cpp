a8376f0e300f66015686e010f8b24cbc0da29975
Nissi39 <92149993+Nissi39@users.noreply.github.com>
2021-10-21 15:24:41 +0530

    Add files via upload

---
+++ 29.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int day;
+	scanf("%d",&day);
+	switch(day)
+	case 1:printf(sunday);
+		break;
+	case 2:monday;
+	break;
+	case 3:wednesday;
+	break;
+	case 4:thursday;
+	break;
+	case 5:friday;
+	break;
+	case 6:saturday;
+	break;
+	case 7:
+}
